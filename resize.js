5af103233b28bd87b144737c48168be287d13971
armeentalwandi <68311871+armeentalwandi@users.noreply.github.com>
2020-07-19 00:00:36 -0400

    d

--- resize.js
+++ resize.js
@@ -1,4 +1,4 @@
 var h = window.innerHeight;
 var w = window.innerWidth;
 //document.body.style.backgroundImage = `url("")`;
-document.body.style.backgroundImage = `url("https://source.unsplash.com/?nature,water")`;
+document.body.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?nature,water")`;
